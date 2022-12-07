0174e0a9aefc68f220a1ba1c0f65d67a47e34390
KJY <KJY@SALTLUX-KIM-JONG-YOUNG>
2022-12-07 16:25:07 +0900

    Spring Eureka Discovery Service

---
+++ src/main/java/edu/jong/app/SpringDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package edu.jong.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryServiceApplication.class, args);
+	}
+
+}
